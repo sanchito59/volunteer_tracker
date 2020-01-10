1cca5f9cbdaf3020e163cc54fe217b6a268d7787
Chris Sanchez <c.sanch7@gmail.com>
2020-01-10 09:32:34 -0800

    add more passing tests, debugging 'expected 9 to be an instance of Integer'

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,32 @@
 class Project
-    attr_accessor :name, :id
+    attr_accessor :title, :id
 
     def initialize(attributes)
-        @name = attributes.fetch(:name)
+        @title = attributes.fetch(:title)
         @id = attributes.fetch(:id)
+    end
+
+    def self.all
+        returned_projects = DB.exec("SELECT * FROM projects;")
+        projects = []
+        returned_projects.each() do |project|
+          title = project.fetch("title")
+          id = project.fetch("id").to_i
+          projects.push(Project.new({:title => title, :id => id}))
+        end
+        projects
+    end
+
+    def ==(project_to_compare)
+        if project_to_compare != nil
+            self.title() == project_to_compare.title()
+        else
+            false
+        end
+    end
+
+    def save
+        result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+        @id = result.first().fetch("id").to_i      
     end
 end
